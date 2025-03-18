c13d3349dbb0fdc01c45227ca1646d96f3ee1ede
marian <marian.waffle@ontariotechu.net>
2025-03-17 20:01:44 -0400

    installed babel, jest, and added a test file

---
+++ footytrack3/jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFiles: ['./jest.setup.js'],
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest'
+    },
+    moduleFileExtensions: ['js', 'jsx'],
+  };
